2f9b67d9b005bd69a161a974b510e999ca7c7e9e
Omar Gamal <omargamal@Omars-MacBook-Pro.local>
2019-08-22 03:46:57 +0200

    remove vuetify

--- frontend/src/app/App.vue
+++ frontend/src/app/App.vue
@@ -1,36 +1,28 @@
 <template>
-    <div class="jumbotron">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-6 offset-sm-3">
-                    <div v-if="alert.message" :class="`alert ${alert.type}`">{{alert.message}}</div>
-                    <router-view></router-view>
-                </div>
-            </div>
-        </div>
-    </div>
+
+    <router-view></router-view>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
+    import {mapState, mapActions} from 'vuex'
 
-export default {
-    name: 'app',
-    computed: {
-        ...mapState({
-            alert: state => state.alert
-        })
-    },
-    methods: {
-        ...mapActions({
-            clearAlert: 'alert/clear' 
-        })
-    },
-    watch: {
-        $route (to, from){
-            // clear alert on location change
-            this.clearAlert();
+    export default {
+        name: 'app',
+        computed: {
+            ...mapState({
+                alert: state => state.alert
+            })
+        },
+        methods: {
+            ...mapActions({
+                clearAlert: 'alert/clear'
+            })
+        },
+        watch: {
+            $route(to, from) {
+                // clear alert on location change
+                this.clearAlert();
+            }
         }
-    } 
-};
+    };
 </script>
